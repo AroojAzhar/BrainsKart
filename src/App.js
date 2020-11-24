877833629dee5619b61e283d6b31e485f9fd0323
AroojAzhar <ansari.arooj32@gmail.com>
2020-11-23 22:55:21 -0500

    big basket

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from "react";
 import './App.css';
-import IMAGE from "../src/assets/events-now.png";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+import ProductList from "./components/products/ProductList";
+import ProductAdmin from "./components/products/ProductAdmin";
+import CreateProduct from "./components/products/CreateProduct";
+import UpdateProduct from "./components/products/UpdateProduct";
 
-class App extends React.Component{
-
-    render() {
-        return (
-            <div className="App">
-                <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
-                    <a href="/" className='navbar-brand'>Github Profile Search App</a>
-                </nav>
-                <h1>Github test</h1>
-                <img src={IMAGE} alt=""/>
-                </div>
-        );
-    }
-}
+let App = () => {
+    return(
+        <React.Fragment>
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route exact path = "/" component={Home}/>
+                    <Route exact path = "/products" component={ProductList}/>
+                    <Route exact path = "/admin" component={ProductAdmin}/>
+                    <Route exact path = "/create-product" component={CreateProduct}/>
+                    <Route exact path = "/update-product/:id" component={UpdateProduct}/>
+                </Switch>
+            </Router>
+        </React.Fragment>
+    );
+};
 export default App;
